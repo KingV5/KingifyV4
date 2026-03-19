eef0ab91ebc692971d6343c01e5b1b1c9acfa7eb
KingV5 <jeremiahelpro@gmail.com>
2026-03-19 16:47:02 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -11,8 +11,8 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
-		end)
+		return game:HttpGet('https://raw.githubusercontent.com/KingV5/KingifyV4/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+end)
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
@@ -42,8 +42,8 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function() 
-		return game:HttpGet('https://github.com/poopparty/poopparty') 
-	end)
+	return game:HttpGet('https://raw.githubusercontent.com/KingV5/KingifyV4/main/loader.lua', true)
+end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 	commit = commit and #commit == 40 and commit or 'main'
